604456cdbb42fad8a2c3e8a990a23a79f1a50791
Mike <mmarceca@aardvarkbrigade.com>
2019-01-10 18:47:22 -0500

    added eslint and made continuous loop until the program is turned off

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 var https = require('https');
 var request = require('request');
 
-// URL to be used for data
-var url = 'https://interview.adpeai.com/api/v1/get-task';
-
-https.get(url, function(res){
-  var body = '';
-  // Appending data together
-  res.on('data', function(chunk){
-      body += chunk;
+// Running the program every two seconds
+setInterval(() => {
+  // URL to be used for data
+  var url = 'https://interview.adpeai.com/api/v1/get-task';
+  https.get(url, function(res){
+    var body = '';
+    // Appending data together
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+    // On the end of appending the data we parse the body and send it along to the post request function
+    res.on('end', function(){
+        var response = JSON.parse(body);
+        postReq(response)
+    });
+  }).on('error', function(err){
+      console.log("Error: ", err);
   });
-  // On the end of appending the data we parse the body and send it along to the post request function
-  res.on('end', function(){
-      var response = JSON.parse(body);
-      postReq(response)
-  });
-}).on('error', function(err){
-    console.log("Error: ", err);
-});
+}, 2000);
 
 function postReq(responseBody) {
   var resultAnswer = getResults(responseBody)
